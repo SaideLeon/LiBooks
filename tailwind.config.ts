eaf9c41bc4842a4a867e31bd6ac21dfe88d8c482
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-11-11 09:01:46 +0000

    Resetar tudo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type { Config } from "tailwindcss"
 
 const config = {
@@ -19,59 +18,6 @@ const config = {
       },
     },
     extend: {
-      colors: {
-        "primary": "hsl(var(--primary))",
-        "secondary": "hsl(var(--secondary))",
-        "background-light": "var(--background-light)",
-        "background-dark": "var(--background-dark)",
-        "text-light": "var(--text-light)",
-        "text-dark": "var(--text-dark)",
-        "text-muted-light": "var(--text-muted-light)",
-        "text-muted-dark": "var(--text-muted-dark)",
-        "card-light": "var(--card-light)",
-        "card-dark": "var(--card-dark)",
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary_shadcn: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary_shadcn: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      fontFamily: {
-        display: ["var(--font-inter)", "sans-serif"],
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
